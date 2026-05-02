8be3c15c782425a5a7a1798812cdf5145358635e
carlos <cgarrote92@gmail.com>
2026-05-02 14:35:07 +0100

    formatting

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -3,61 +3,61 @@
 
 int main(int ac, char *av[])
 {
-    if (CREATE_TEST == 1)
-    {
-	create_files();
-	return (0);
-    }
-
-    if (ac != 4)
-    {
-	std::cout << "Error: Invalid number of arguments." << std::endl;
-	return 1;
-    }
-    
-    std::ifstream inFile;
-    std::ofstream outFile;
-
-    std::string input = av[1];
-
-    inFile.open(input.c_str());
-    
-
-    if (inFile.fail())
-    {
-	std::cout << "Error opening input file" << std::endl;
-	return 1;
-    }
-
-    std::string output = input + ".replace";
-
-    outFile.open(output.c_str());
-
-    if (outFile.fail())
-    {
-	std::cout << "Error opening outfile file" << std::endl;
-	return 1;
-    }
-
-    std::string line;
-    std::string s1 = av[2];
-    std::string s2 = av[3];
-
-    while (std::getline(inFile,line))
-    {
-	size_t pos = line.find(s1);
-	while (pos != std::string::npos)
+	if (CREATE_TEST == 1)
 	{
-	    line.erase(pos, s1.length());
-	    line.insert(pos, s2);
-	    pos = line.find(s1, pos + s2.length());
+		create_files();
+		return (0);
 	}
-	outFile << line << std::endl;
-    }
 
-    inFile.close();
-    outFile.close();
+	if (ac != 4)
+	{
+		std::cout << "Error: Invalid number of arguments." << std::endl;
+		return 1;
+	}
+	
+	std::ifstream inFile;
+	std::ofstream outFile;
 
-    return 0;
+	std::string input = av[1];
+
+	inFile.open(input.c_str());
+	
+
+	if (inFile.fail())
+	{
+		std::cout << "Error opening input file" << std::endl;
+		return 1;
+	}
+
+	std::string output = input + ".replace";
+
+	outFile.open(output.c_str());
+
+	if (outFile.fail())
+	{
+		std::cout << "Error opening outfile file" << std::endl;
+		return 1;
+	}
+
+	std::string line;
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+
+	while (std::getline(inFile,line))
+	{
+		size_t pos = line.find(s1);
+		while (pos != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos = line.find(s1, pos + s2.length());
+		}
+		outFile << line << std::endl;
+	}
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
 }
 
