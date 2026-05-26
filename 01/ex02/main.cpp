2016d8c2d779405012b5b3d099f8eeb4cb79d13a
carlos <cgarrote92@gmail.com>
2026-05-26 20:01:28 +0100

    fix cpp01,ex02

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -8,15 +8,15 @@ int main()
     std::string &stringREF = str;
 
     // print memory address of: string variable, stringPTR and stringREF
-    std::cout << std::addressof(str) << " -> memory address of string variable" << std::endl;
-    std::cout << std::addressof(stringPTR) << " -> memory address of stringPTR" << std::endl;
-    std::cout << std::addressof(stringREF) << " -> memory address of stringREF" << std::endl;
+    std::cout << &str << " -> memory address of string variable" << std::endl;
+    std::cout << stringPTR << " -> memory address of stringPTR" << std::endl;
+    std::cout << &stringREF << " -> memory address of stringREF" << std::endl;
 
     std::cout << "---" << std::endl;
 
     // print value of: string variable, value pointed by stringPTR, value pointed by stringREF
     std::cout << str << " -> value of string variable" << std::endl;
-    std::cout << stringPTR << " -> value pointed by stringPTR" << std::endl;
+    std::cout << *stringPTR << " -> value pointed by stringPTR" << std::endl;
     std::cout << stringREF << " -> value pointed by stringREF" << std::endl;
 
     return 0;
